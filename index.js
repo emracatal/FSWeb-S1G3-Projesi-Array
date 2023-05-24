8f5431752e1520888a04682e95609582556d30f0
emracatal <134175723+emracatal@users.noreply.github.com>
2023-05-24 17:32:13 +0300

    mesaj

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* BU DERSDEKİ TÜM DEĞERLERİN DÖNMESİNİ (RETURN) UNUTMAYIN, AKSİ TAKDİRDE TEST KONTROLÜ ÇALIŞMAZ */
+/*Yapılması gerekenler
+index.js dosyasını bul ve fonskiyondan geri gelen değerin(return) beklenen gibi görünene kadar görevleri tamamlamaya çalış.
+
+.forEach(), .map(), .filter() ve .reduce() metodlarını kullanmak şimdilik yasak. Bu proje özelinde bunların hiçbirini kullanma!
+*/
 
 /* İşte veriniz bu */
 const orijinalTatlar = [
@@ -29,146 +34,151 @@ const orijinalTatlar = [
   "Vanilya",
 ];
 
-/* Görev 1: Diziyi kopyalama!
-Orijinal tatların bulunduğu toplam 25 aromanın olduğu bir dizimiz var (yukarıya bakın). Bu görevde, bu dizi'ye veri yazma ve bu diziden veri okuma işlemleri yapacağız.
-Tüm bu değişiklikleri yaparken, gerçek, orijinal 25 lezzetin listesini kaybetmek istemiyoruz. Bu yüzden orijinal diziyi kopyalamamız gerekmektedir!
-
+/* Görev 1: Diziyi kopyalama! 
+Orijinal tatların bulunduğu toplam 25 aromanın olduğu bir dizimiz var (yukarıya bakın). 
+Bu görevde, bu dizi'ye veri yazma ve bu diziden veri okuma işlemleri yapacağız.
+Tüm bu değişiklikleri yaparken, gerçek, orijinal 25 lezzetin listesini kaybetmek istemiyoruz. 
+Bu yüzden orijinal diziyi kopyalamamız gerekmektedir!
 /*
 Aşağıdakileri yapmak için aşağıdaki kopyalama işlevini kullanın:
    1. parametre olarak bir dizi alın - işlev çağrıldığında orijinalTatlar'i argüman olarak ileteceksiniz.
    2. Alınan dizinin bir kopyasını döndür
 */
 
-
-function kopyala(/*kod buraya*/){
-  /*kod buraya*/
+function kopyala(kopyalananDizi){
+  const yeniDizi = [...kopyalananDizi]
+  return yeniDizi;
 }
+console.log ("kopyalama sorusu",kopyala(orijinalTatlar));
 
-
-/* Görev 2:
+/* Görev 2: ARRAY.LENGTH
 Bir dizinin tam olarak 25 çeşit olduğunu onaylayın. İşleviniz şunları kabul etmelidir:
    1. parametre olarak bir dizi
    2. Verilen dizinin 25 çeşit olup olmadığını kontrol edin
    3. İşleviniz, dizinin uzunluğu 25 ise DOĞRU(true) ve dizinin uzunluğu 25 DEĞİLSE YANLIŞ(false) bir boolean döndürmelidir.
-
-
-Örneğin: dizi25Cesit(orijinalTatlar) kodunuz düzgün çalışıyorsa true değerini döndürür.
+   Örneğin: dizi25Cesit(orijinalTatlar) kodunuz düzgün çalışıyorsa true değerini döndürür.
 */
 
+function dizi25Cesitmi(diziBoyu){
+  if(dizi25Cesitmi.length === 25) {
+    return true;
+  } else {
+    return false;
+  }
+  }
+  console.log(dizi25Cesitmi(orijinalTatlar));
 
-function dizi25Cesitmi(/*kod buraya*/){
-  /*kod buraya*/
-}
-
-
-/* Görev 3:
-Pastane sahibi size yeni bir lezzet fikriyle geldi: Kakule! Bunun da çok tutacağından çok emin. Bu lezzeti eklemek için diziyi değiştirmeniz gerekir.
-
+/* Görev 3: ARRAY.UNSHIFT
+Pastane sahibi size yeni bir lezzet fikriyle geldi: Kakule! Bunun da çok tutacağından çok emin. 
+Bu lezzeti eklemek için diziyi değiştirmeniz gerekir.
 Aşağıdakileri yapmak için cesitEkle işlevini kullanın:
    1. İlk parametrede tatlar dizisini argüman olarak alan bir dizi alın
    2. İkinci parametrede yeni tadı 'string' argümanı olarak alın
    3. İşlev, argüman olarak verilen yeni tadı, verilen dizinin başına ekler.
    4. İşlev, yeni oluşan diziyi döndürmelidir
-
   Örneğin: cesitEkle(orijinalTatlar, "Kakule") işlevi doğru çalıştığında ["Kakule", "Muz",..."Vanilya"] şeklinde dönmelidir
 */
 
-
-function cesitEkle(/*kod buraya*/){
-  /*kod buraya*/
+function cesitEkle(tatlarDizisi,yeniCesit){
+  tatlarDizisi.unshift(yeniCesit);
+  return tatlarDizisi;
 }
+console.log (cesitEkle(orijinalTatlar, "Kakule"));
 
+/*KAYITTAN KONTROL ET
+function cesitEkle(tatlarDizisi,yeniCesit){
+  const yeniDizi=[yeniCesit, ..tatlarDisizi]
+}
+console.log (cesitEkle(orijinalTatlar, "Kakule"));
+*/
 
-/* Cörev 4:
-
-Hoooppppalaa! orijinalTatlar dizisinde artık 26 aroma var! Göreviniz, dizinin sonundan bir öğeyi kaldırmaktır.
-
+/* Cörev 4: ARRAY.POP
+Hoooppppalaa! orijinalTatlar dizisinde artık 26 aroma var! Göreviniz, dizinin sonundan bir öğeyi kaldırmaktır. 
 Aşağıdakileri yapmak için sonCesitiKaldir işlevini kullanın:
    1. Bir dizi alın
    2. Alınan diziden son öğeyi kaldırın
    3. Ortaya çıkan diziyi döndürün
-
    Örneğin: sonCesitiKaldir(orijinalTatlar) çalıştırıldığında ["Kakule", "Muz",..."Çilek"] döndürülür.
 */
 
-
-function sonCesitiKaldir(/*kod buraya*/){
-  /*kod buraya*/
+function sonCesitiKaldir(tatlarDizisi){
+  tatlarDizisi.pop()
+  return tatlarDizisi;
 }
+console.log(sonCesitiKaldir(orijinalTatlar));
 
-
-/* Görev 5:
+/* Görev 5: ARRAY[#]
 Dizideki belirli bir indeksteki çeşniyi döndüren bir işlev yazın.
-
 Aşağıdakileri yapmak için aşağıdaki indekstekiCesitiGetir işlevini kullanın:
    1. İlk parametrede tatlar dizisini argüman olarak alacak bir dizi alın
    2. İkinci parametrede argüman olarak istenen dizini alacak bir sayı alın
    3. Verilen indekste bulunan çeşiti döndürün
-
-   Örneğin: indekstekiCesitiGetir(orijinalTatlar, 2) çalıştırılmasıyla, Kakule'in başarıyla eklendiği varsayarsak sonuç "Ceviz" olucaktır.
+   Örneğin: indekstekiCesitiGetir(orijinalTatlar, 2) çalıştırılmasıyla, 
+   Kakule'in başarıyla eklendiği varsayarsak sonuç "Ceviz" olucaktır.
 */
 
-function indekstekiCesitiGetir(/*kod buraya*/){
-  /*kod buraya*/
+function indekstekiCesitiGetir(tatlarDizisi,sira){
+  return tatlarDizisi[2];
 }
+console.log(indekstekiCesitiGetir(orijinalTatlar, 2));
 
-
-/* Görev 6:
-
-Firma, ürün yelpazesine daha fazla lezzet eklemek istediğinden, sadece ilk veya son aromayı keyfi olarak çıkarmak yerine, aroma adına göre aromaları kaldırmaları gerektiğini fark ederler. Göreviniz, verilen bir dizinde, ada göre bu tadı diziden çıkarmak.
-
+/* Görev 6: ARRAY.SPLICE & text.INDEXOF
+Firma, ürün yelpazesine daha fazla lezzet eklemek istediğinden, sadece ilk veya son aromayı keyfi olarak çıkarmak yerine, 
+aroma adına göre aromaları kaldırmaları gerektiğini fark ederler. Göreviniz, verilen bir dizinde, ada göre bu tadı diziden çıkarmak.
 Aşağıdakileri yapmak için ismeGoreCesitCikar işlevini kullanın:
    1. İlk parametrede tatlar dizisini argüman olarak alın
    2. İkinci parametrede, lezzet adını argüman olarak alın
    3. Alınan diziden alınan aromayı kaldırın
    4. Artık bir tane daha az lezzet içeren sonuçtaki diziyi döndürün
-
   Örneğin:  ismeGoreCesitCikar(orijinalTatlar, "Tarçın") çalıştırılmasıyla,  "Tarçın" aroması kaldırılacağından uzunluğu 24 olan bir dizi döndürülür.
-
   İPUCU: Bunun için .splice() kullanabilirsiniz.
 */
 
-function ismeGoreCesitCikar(/*kod buraya*/){
-  /*kod buraya*/
+function ismeGoreCesitCikar(tatlarDizisi,cesitAdi){
+  const sirasi = tatlarDizisi.indexOf(cesitAdi);
+//fonksiyon içine de console.log yazılabiliyor
+  console.log(sirasi);
+  tatlarDizisi.splice(sirasi,1);
+  return tatlarDizisi;
 }
 
+console.log(ismeGoreCesitCikar(orijinalTatlar, "Tarçın"));
 
-/* Görev 7:
-
+/* Görev 7: ARRAY.includes
 14 Şubat Sevgililer Günü'nde Firma, tüm çikolata lezzetlerini öne çıkaran bir promosyon malzemeleri oluşturmak istiyor.
-Göreviniz, dizideki her bir öğeyi kontrol eden ve yalnızca verilen isimin geçtiği lezzetleri bulup orataya çıkan yeni diziyi döndüren, ismeGoreFiltrele adlı bir işlev yazmaktır.
+Göreviniz, dizideki her bir öğeyi kontrol eden ve yalnızca verilen isimin geçtiği lezzetleri bulup orataya çıkan yeni diziyi döndüren, 
+ismeGoreFiltrele adlı bir işlev yazmaktır.
 Bu, farklı tatil ve kutlamalarda "Fındık", "Fıstık", "Çikolata" vb. lezzetler için filtreleme yapabilmenizi sağlar.
-
 Aşağıdakileri yapmak için ismeGoreFiltrele işlevini kullanın:
    1. İlk parametrede tatlar dizisini argüman olarak alacak bir dizi alın
    2. İkinci parametrede filtre değerini argüman olarak alın (örnek: "Çikolata")
    3. Dizideki tatlardan herhangi birinin filtre değerini içerip içermediğini kontrol edin.
    4. Varsa, onları yeni bir diziye ekleyin
    5. Filtrelenmiş tatları içeren yeni diziyi döndürün
-
   Örneğin: ismeGoreFiltrele(orijinalTatlar, "Çikolata") çalıştırıldığında ["Çikolata", "Fındık Çikolata", "Fıstık Çikolata", "Badem Çikolata"]
-
   İPUCU - bunu çözmenize yardımcı olması için .includes yöntemini kullanabilirsiniz.
-
   Bu sorunu çözmek için GELİŞMİŞ DİZİ YÖNTEMLERİNİ (yani .filter) KULLANMAYIN.
 */
 
-
-function ismeGoreFiltrele(/*kod buraya*/){
-  /*kod buraya*/
+function ismeGoreFiltrele(tatlarDizisi,filtre){
+  const filtrelenmisTatlar = []
+  for (let i=0;i<tatlarDizisi.length;i++){
+    if(tatlarDizisi[i].includes(filtre)){
+      filtrelenmisTatlar.push(tatlarDizisi[i]);
+    }
+  }  
+  return filtrelenmisTatlar;
 }
+console.log(ismeGoreFiltrele(orijinalTatlar, "Çikolata"));
 
-
-
-/* ALIŞTIRMA */
-
-/* ALIŞTIRMA 1: Bir dizideki ortalama kelime sayısını döndüren bir fonksiyon yazın. Bu işlevi orijinalTatlar dizisinde test etmelisiniz, ancak bunu herhangi bir dizi için de kullanabilmelisiniz.
-
+/* ALIŞTIRMA */ 
+/* ALIŞTIRMA 1: ORTALAMA KELİME SAYISI?
+Bir dizideki ortalama kelime sayısını döndüren bir fonksiyon yazın. 
+Bu işlevi orijinalTatlar dizisinde test etmelisiniz, ancak bunu herhangi bir dizi için de kullanabilmelisiniz.
 Aşağıdakileri yapmak için ortalamaKelimeSayisi işlevini kullanın:
    1. orijinalTatlar dizisini alın
    2. Dizideki öğe başına kaç kelime sayın
    3. Dizideki öğe başına ortalama kelime sayısını döndürün
-
    Örneğin: ortalamaKelimeSayisi(orijinalTatlar) 0 ile 2 arasında bir sayı döndürmelidir.
 */
 
@@ -178,21 +188,23 @@ function ortalamaKelimeSayisi(/*kod buraya*/){
 
 
 /* ALIŞTIRMA 2:
-Firma mevcut tatların yanında artık mevsimlik lezzetler ve hatta bölgesel lezzetler de sunmaktadır. Toplam 25 lezzet aromasını
-orijinalTatlar, yeniTatlar, mevsimlikTatlar ve bolgeselTatlar'dan rastgele seçecek ve bunu rastgeleTatlar adlı bir dizide saklayan bir fonksiyon yazın.
+Firma mevcut tatların yanında artık mevsimlik lezzetler ve hatta bölgesel lezzetler de sunmaktadır. 
+Toplam 25 lezzet aromasını orijinalTatlar, yeniTatlar, mevsimlikTatlar ve bolgeselTatlar'dan rastgele seçecek 
+ve bunu rastgeleTatlar adlı bir dizide saklayan bir fonksiyon yazın.
 
 Aşağıdakileri yapmak için rastgeleTatlar işlevini ve yeni dizileri kullanın:
    1. Tüm farklı tatlara sahip dört diziyi alın (orijinalTatlar yukarıda, diğerleri aşağıda)
    2. Dört diziden rastgele tatlar seçin
    3. Uzunluğu 25 olan rastgeleTatlar adlı yeni bir dizi döndürün
-
-  Örneğin: rastgeleTatlar(orijinalTatlar, yeniTatlar, mevsimlikTatlar, bolgeselTatlar) çalıştırıldığında ["Kestane", "Ballı Badem,"..."Hindistan Cevizi", "Kuru üzüm"].
+  Örneğin: rastgeleTatlar(orijinalTatlar, yeniTatlar, mevsimlikTatlar, bolgeselTatlar) çalıştırıldığında 
+  ["Kestane", "Ballı Badem,"..."Hindistan Cevizi", "Kuru üzüm"].
 */
 
 
 function rastgeleTatlar(/*kod buraya*/){
   /*kod buraya*/
 }
+
 
 // NEW DATA ARRAYS FOR STRETCH 2 ⬇️
 // const yeniTatlar = [
